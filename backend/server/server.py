600536707087f2056745d8e102f09b203cc9a599
ramanveerji <ramandeepmatharu@gmail.com>
2025-04-09 17:30:47 +0530

    cors issue fix

--- backend/server/server.py
+++ backend/server/server.py
@@ -85,7 +85,8 @@ manager = WebSocketManager()
 # Middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
+    #allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
